ee8eca50486395c53c44701fd518ec8545f7e86e
Harkirat23 <43305175+Harkirat23@users.noreply.github.com>
2018-09-18 18:22:52 -0700

    Add files via upload

---
+++ Discount/Discount/Discount.cpp
@@ -0,0 +1,37 @@
+//This program calculates the price of an item before and after a discount
+
+#include <iosteram>
+#include <iomanip>
+
+using namespace std; 
+
+int main()
+{
+	//Constant for discount percentage
+	const double DISCOUNT_RATE = 0.15;
+
+	//variables
+	price,	//price of the item
+	discount,	//discounted price of item
+	items,	//number of items purchased
+	total, //total price of the number of items purchased
+	disctotal, //the total price of the number of items after discount 
+	discamount, //the amount of discount applies
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
